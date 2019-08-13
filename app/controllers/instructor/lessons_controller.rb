6e9ebd00358a42439bfdeddeac4d1f8b08300cec
herangakr <ma.muthukuda@gmail.com>
2019-08-13 23:54:28 +0000

    Video Uploading

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,5 +1,5 @@
 class Instructor::LessonsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :require_authorized_for_current_section
 
   def new
@@ -15,7 +15,7 @@ class Instructor::LessonsController < ApplicationController
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      return render plain: 'Unauthorized', status: :unauthorized
+      render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
@@ -25,6 +25,6 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle)
+    params.require(:lesson).permit(:title, :subtitle, :video)
   end
 end
