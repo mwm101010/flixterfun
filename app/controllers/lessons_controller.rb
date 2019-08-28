723979d543b372db163595804db4c38ac8616017
herangakr <ma.muthukuda@gmail.com>
2019-08-28 00:38:15 +0000

    Final touches

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,6 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course, only: [:show]
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
    
@@ -8,9 +8,9 @@ class LessonsController < ApplicationController
 
   private
 
-  def require_authorized_for_current_course
-    if current_lesson.section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
+  def require_authorized_for_current_lesson
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled in the course to view this page'
     end
   end
 
